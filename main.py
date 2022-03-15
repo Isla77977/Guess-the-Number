32320de5e9176c0c5456f56c2557f1b9aaed9d6b
Isla77977 <99439965+Isla77977@users.noreply.github.com>
2022-03-15 01:06:51 +0000

    Added a basic life system.

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
-#Guessing game v1:
+#Guessing game v2: Adding the Lives System and Higher or Lower
 import random
 answer = 0
 guess = 11
+lives = 3
 #Ask the user their name.
-str_name = input("Welcome! Please enter your name: ")
+str_name = input("Welcome! Please enter your name: ").strip().title()
 #Create Random Number Generator.
-answer = random.randrange(0, 10)
+answer = random.randrange(1, 10)
 #Allow user to start guessing.
-print("Okay, your task is to guess a number from 0-10. Good luck!")
+print("Okay, your task is to guess a number from 1-10. Good luck!")
 while guess != answer:
   guess = int(input("Guess: "))
   #If they get it right end game, if they get it wrong continue game.
@@ -16,3 +17,18 @@ while guess != answer:
     break
   else:
     print("INCORRECT")
+    lives -= 1
+  if lives <= 0:
+    print("I'm sorry, you have run out of lives. The correct number was {}".format(answer))
+    break
+
+
+
+#Things to add:
+# -Lives system
+# -Play again
+# -Error catching
+# -A way to clear past guesses (while still keeping guess list)
+# -Guesses being displayed
+# -Different difficulties
+# -Add higher or lower
