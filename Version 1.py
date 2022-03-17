fee35e9b9aafbb61acb2fcbb076a3ffd6180e1d5
Isla77977 <99439965+Isla77977@users.noreply.github.com>
2022-03-17 20:12:25 +0000

    Added better comments to V1

--- Version 1.py
+++ Version 1.py
@@ -1,18 +1,22 @@
 #Guessing game v1:
+#Import random creates a randomizer that will give a number when it's used right.
 import random
+#These make it so the game runs since they're different (refer to line 15-20)
 answer = 0
 guess = 11
-#Ask the user their name.
+#Asks the user their name and storing it in a variable.
 str_name = input("Welcome! Please enter your name: ")
-#Create Random Number Generator.
+#Creates Random Number Generator using the 'random' that was imported earlier.
 answer = random.randrange(0, 10)
-#Allow user to start guessing.
+#Allows user to guess as long as their guess is not the same as the answer.
 print("Okay, your task is to guess a number from 0-10. Good luck!")
 while guess != answer:
   guess = int(input("Guess: "))
-  #If they get it right end game, if they get it wrong continue game.
+  #If they get it right end game. If they get it wrong, continue game.
   if guess == answer:
     print("Congratulations, {}. You guessed the correct number! It was {}.".format(str_name, answer))
     break
   else:
     print("INCORRECT")
+
+#No error catching, difficulties, lives.
